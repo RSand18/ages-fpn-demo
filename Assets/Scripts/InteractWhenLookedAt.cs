d9a8b47f0de2738380873a33b42577acb4ad2388
RSand18 <43037672+RSand18@users.noreply.github.com>
2019-03-21 14:24:07 -0500

    Door!

---
+++ Assets/Scripts/InteractWhenLookedAt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWhenLookedAt : MonoBehaviour
+{
+    private IInteractive lookedAtInteractive;
+    void Update()
+    {
+        if (Input.GetButtonDown("interact") && lookedAtInteractive != null)
+        {
+            Debug.Log("Player pressed the interact button.");
+            lookedAtInteractive.InteractWith();
+        }
+    }
+
+    ///<summary>
+    ///Event handler for DetectLookedAtInteract,LookedAtInteractiveChanged
+    /// </summary>
+    
+    private void OnLookedAtinteractiveChanged(IInteractive newLookedAtInteractive)
+    {
+        lookedAtInteractive = newLookedAtInteractive;
+    }
+
+    #region Event subscription / unsubscription
+    private void OnEnable()
+    {
+        DetectLookedAtInteractive.LookedAtInteractiveChanged += OnLookedAtinteractiveChanged;
+    }
+    private void OnDisable()
+    {
+        DetectLookedAtInteractive.LookedAtInteractiveChanged -= OnLookedAtinteractiveChanged;
+    }
+    #endregion
+}
