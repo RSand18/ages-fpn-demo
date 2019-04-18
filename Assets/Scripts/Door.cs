79dc5e51c2e9ce5c8ff276e08c36c7495e95376d
RSand18 <43037672+RSand18@users.noreply.github.com>
2019-04-18 16:53:44 -0500

    Added locked door script

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,6 +6,40 @@ using UnityEngine;
 
 public class Door : InteractiveObject
 {
+    [Tooltip("Check this box to lock the door.")]
+    [SerializeField]
+    private bool isLocked;
+
+    [Tooltip("The text that displays when the player looks at the door while it's locked.")]
+    [SerializeField]
+    private string lockedDisplayText = "Locked";
+
+    [Tooltip("Play this audio clip when the player interacts with a locked door without owning the key.")]
+    [SerializeField]
+    private AudioClip lockedAudioClip;
+
+    [Tooltip("Play this audio clip when the player opens the door.")]
+    [SerializeField]
+    private AudioClip openAudioClip;
+
+    public override string DisplayText => isLocked ? lockedDisplayText : base.displayText;
+
+    // This is an alternative way to express the same logic as above.
+    //public override string DisplayText
+    //{
+    //    get
+    //    {
+    //        if (isLocked)
+    //        {
+    //            return lockedDisplayText;
+    //        }
+    //        else
+    //        {
+    //            return base.DisplayText;
+    //        }
+    //    }
+    //}
+
     private Animator animator;
     private bool isOpen = false;
     private int shouldOpenAnimParameter = Animator.StringToHash(nameof(shouldOpenAnimParameter));
@@ -27,10 +61,18 @@ public class Door : InteractiveObject
     {
         if (!isOpen)
         {
-            base.InteractWith();
-            animator.SetBool(shouldOpenAnimParameter, true);
-            displayText = string.Empty;
-            isOpen = true;
+            if (!isLocked)
+            {
+                audioSource.clip = openAudioClip;
+                animator.SetBool(shouldOpenAnimParameter, true);
+                displayText = string.Empty;
+                isOpen = true;
+            }
+            else // if the door is locked...
+            {
+                audioSource.clip = lockedAudioClip;
+            }
+            base.InteractWith(); // This plays a sound effect!
         }
     }
 }
